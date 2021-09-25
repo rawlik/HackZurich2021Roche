12c74bede8d821fc102f2d9403c348bc00158927
Michał Rawlik <michal.rawlik@psi.ch>
2021-09-25 13:33:04 +0200

    Set up a question manager.

---
+++ UnityFrontend/Assets/Scripts/QuestionManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestionManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text questionText;
+    [SerializeField] private VerticalLayoutGroup answerButtonsGroup;
+    [SerializeField] private GameObject answerButtonPrefab;
+
+    private void OnValidate()
+    {
+        if (questionText == null)
+            Debug.LogError("questionText cannot be null");
+        if (answerButtonsGroup == null)
+            Debug.LogError("answerButtonsGroup cannot be null");
+        if (answerButtonPrefab == null)
+            Debug.LogError("answerButtonPrefab cannot be null");
+    }
+
+    private void setQuestion(string question)
+    {
+        questionText.text = question;
+    }
+
+    private void cleanAnswers()
+    {
+        foreach (Transform answerButton in answerButtonsGroup.transform)
+        {
+            Destroy(answerButton.gameObject);
+        }
+    }
+
+    private void setAnswers(IEnumerable<string> answers)
+    {
+        cleanAnswers();
+
+        foreach (var answer in answers)
+        {
+            var answerButton = Instantiate(answerButtonPrefab);
+            answerButton.transform.parent = answerButtonsGroup.transform;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<string> testAnswers = new List<string>();
+        testAnswers.Add("testAnswer1");
+        testAnswers.Add("testAnswer2");
+        setAnswers(testAnswers);
+    }
+}
